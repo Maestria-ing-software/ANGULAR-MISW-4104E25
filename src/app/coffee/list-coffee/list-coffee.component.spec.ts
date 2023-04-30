3079220384d78ddb6a2a901e8504cf85021bd692
carlos castillo <carlos.castillo@zinobe.com>
2023-04-30 02:13:51 -0400

    prueba de componente

--- src/app/coffee/list-coffee/list-coffee.component.spec.ts
+++ src/app/coffee/list-coffee/list-coffee.component.spec.ts
@@ -4,15 +4,20 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ListCoffeeComponent } from './list-coffee.component';
+import { CoffeeService } from '../coffee.service';
+import { COFFEES } from '../coffee-mocks';
+import { of } from 'rxjs';
 
 describe('ListCoffeeComponent', () => {
   let component: ListCoffeeComponent;
   let fixture: ComponentFixture<ListCoffeeComponent>;
+  let coffeeService: CoffeeService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ ListCoffeeComponent ]
+      declarations: [ ListCoffeeComponent ],
+      providers: [CoffeeService]
     })
     .compileComponents();
   }));
@@ -20,10 +25,18 @@ describe('ListCoffeeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListCoffeeComponent);
     component = fixture.componentInstance;
+    coffeeService = TestBed.inject(CoffeeService);
+    spyOn(coffeeService, 'getCoffees').and.returnValue(of(COFFEES));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display a table with three rows plus header', () => {
+    const tableRows = fixture.nativeElement.querySelectorAll('tr');
+    expect(tableRows.length).toBe(4); // tres filas más el encabezado.
+  });
+
 });
