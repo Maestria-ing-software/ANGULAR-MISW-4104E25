8436590db8f17a4f1746f405b27ddf4754e6cd95
carlos castillo <carlos.castillo@zinobe.com>
2023-04-30 01:26:30 -0400

    Modificación del componente para listar cafés

--- src/app/coffee/list-coffee/list-coffee.component.ts
+++ src/app/coffee/list-coffee/list-coffee.component.ts
@@ -9,11 +9,23 @@ import { CoffeeService } from '../coffee.service';
 })
 export class ListCoffeeComponent implements OnInit {
   coffees: Array<Coffee> = [];
+  totalCafesOrigen: number = 0;
+  totalCafesBlend: number = 0;
+
   constructor(private coffeeService: CoffeeService) { }
   getCoffees(): void {
     this.coffeeService.getCoffees().subscribe(
       (coffees) =>{
         this.coffees = coffees;
+
+        for (let coffee of this.coffees) {
+          if (coffee.tipo === 'Café de Origen') {
+            this.totalCafesOrigen++;
+          } else if (coffee.tipo === 'Blend') {
+            this.totalCafesBlend++;
+          }
+        }
+
       }
     )
   }
